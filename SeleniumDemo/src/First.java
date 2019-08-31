76ab06d9d84b8716420d444231366b3def7b6928
SRakshith <SRakshith@DSK337-B1.quinnox.corp>
2019-08-31 16:57:08 +0530

    First initial commit

---
+++ SeleniumDemo/src/First.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+public class First {
+
+	public static void main(String[] args) {
+		//declaration and instantiation of objects/variables
+		System.setProperty("webdriver.chrome.driver", "D:\\Rakshith\\Driver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//Launch website
+		driver.navigate().to("http://www.google.com/");
+		
+		//click on the search text box and send value
+		driver.findElement(By.name("q")).sendKeys("quinnox bangalore");
+		
+		//Click on the search button
+		driver.findElement(By.name("btnK")).click();
+	}
+
+}
